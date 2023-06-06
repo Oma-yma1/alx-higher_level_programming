792d23c61f739abf17d30166ffb3b4fd3753bc23
Oma-yma1 <zgani.omayma@gmail.com>
2023-06-06 16:03:17 -0700

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    str_char = ""
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            up_char = chr(ord(char) - 32)
+        else:
+            up_char = char
+        str_char += up_char
+    print("{}".format(str_char))
