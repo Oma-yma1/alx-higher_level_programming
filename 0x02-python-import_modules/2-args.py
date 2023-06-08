7c75d83a4a0cf2d8a588e6d45b1fcce3584b28a2
Oma-yma1 <zgani.omayma@gmail.com>
2023-06-08 11:15:58 -0700

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    argc = len(argv)
+    if argc == 0:
+        print("{} arguments.".format(argc))
+    elif argv == 1:
+        print("{} argument:".format(argc))
+        print("1 : {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(argc))
+        for i in range(0, argc):
+            print("{} : {}".format(i + 1, argv[i]))
+
+
