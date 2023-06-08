bbeb076ecce16033919888d61b655cc3c34705b8
Oma-yma1 <zgani.omayma@gmail.com>
2023-06-08 12:05:40 -0700

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     sum = 0
     argv = sys.argv[1:]
     argc = len(argv)
-    for i in range (argc):
+    for i in range(argc):
         sum = sum + int(argv[i])
     print(sum)
