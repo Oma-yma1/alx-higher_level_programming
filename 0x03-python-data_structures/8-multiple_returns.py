3e9ff9280408b545531b5a3c332199c228c9a91a
Oma-yma1 <zgani.omayma@gmail.com>
2023-06-11 12:59:37 -0700

    task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        length = 0
+        length = len(sentence)
         first = None
     else:
         length = len(sentence)
