baa342dd7d0f47f87512750a4d8d379c44e8a7b2
Oma-yma1 <zgani.omayma@gmail.com>
2023-06-26 07:15:02 -0700

    task3

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    lis_nw = []
+    for j in range(list_length):
+        try:
+            result = float(my_list_1[j]) / float(my_list_2[j])
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        except ValueError:
+            result = 0
+        finally:
+            lis_nw.append(result)
+    return (lis_nw)
