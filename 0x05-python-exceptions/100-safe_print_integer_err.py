e52df5ba007539816acd892b7afd8a8121a7e717
Oma-yma1 <zgani.omayma@gmail.com>
2023-06-26 08:53:07 -0700

    task100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return (True)
     except Exception as k:
-        sys.stderr.write("Exception :{}\n".format(k))
+        sys.stderr.write("Exception: {}\n".format(k))
         return (False)
