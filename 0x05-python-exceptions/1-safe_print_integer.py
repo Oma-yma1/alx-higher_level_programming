a5106d661132b72b6b9e8811f2cfba0d32ea9bcd
Oma-yma1 <zgani.omayma@gmail.com>
2023-06-26 06:02:00 -0700

    task1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,5 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
-        return (False)
-    else:
-        return (True)
+    except ValueError:
+        pass
