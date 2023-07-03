14776ddea7c98bdc47e6b64c15523e93aa00cce3
Oma-yma1 <zgani.omayma@gmail.com>
2023-07-03 08:46:55 -0700

    task2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Class Rectangle"""
+
+
+class Rectangle:
+    """Define a Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return rectangle area"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Return perimeter"""
+        return 2 * (self.width + self.height)
