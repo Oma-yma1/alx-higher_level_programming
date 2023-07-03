58c9120c7366bf18cec93399508859fc36d01441
Oma-yma1 <zgani.omayma@gmail.com>
2023-07-03 07:43:24 -0700

    task1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
     @property
     def width(self):
         """Retrieve the width of rectangle"""
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
