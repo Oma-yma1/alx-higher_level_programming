1775bab3e18a23e875e1db1a8e83d61e706fa8da
Oma-yma1 <zgani.omayma@gmail.com>
2023-07-04 07:54:11 -0700

    task00

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class LockedClass"""
+
+
+class LockedClass:
+    __slots__ = ["first_name"]
+
+    def __setattr__(self, att, val):
+        """setattr method"""
+        if hasattr(self, att):
+            super().__setattr__(att, val)
+        elif att == "first_name":
+            super().__setattr__(att, val)
+        else:
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(att))
