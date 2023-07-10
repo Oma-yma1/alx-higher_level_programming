7f999082dd1e6dfe73d82dcc9bfb5e8fdf3b5f31
Oma-yma1 <zgani.omayma@gmail.com>
2023-07-10 08:29:16 -0700

    ta6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """raises Exception with the mess area() is not implemented"""
+        raise Exception("area() is not implemented")
