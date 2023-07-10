840380d11005df89557bb098a7f2e7b2d3966ed0
Oma-yma1 <zgani.omayma@gmail.com>
2023-07-10 08:13:37 -0700

    ta3

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Exact same object"""
+
+
+def is_same_class(obj, a_class):
+    """returns True if the object is exactly an
+    instance of the specified class"""
+    return type(obj) is a_class
