bcb36dfaf1a8e3e8da39ff62789203479d4d3693
Oma-yma1 <zgani.omayma@gmail.com>
2023-07-11 10:07:57 -0700

    task12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""triangle pascale"""
+
+
+def pascal_triangle(n):
+    """pascal triangle"""
+    if n <= 0:
+        return[]
+
+    triangle = [[1]]
+    for j in range(1, n):
+        r = [1]
+        for k in range(1, j):
+            r.append(triangle[j-1][k-1] + triangle[j-1][k])
+        r.append(1)
+        triangle.append(r)
+
+    return triangle
