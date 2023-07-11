d56d90b1fb46ad74cb60f21459b8164c3b1042f4
Oma-yma1 <zgani.omayma@gmail.com>
2023-07-11 10:40:42 -0700

    task11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -17,4 +17,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """return string"""
-        return"[Square] {}/{}".format(self.__size, self.__size)
+        return "[Square] {}/{}".format(self.__size, self.__size)
