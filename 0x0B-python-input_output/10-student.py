c83cfdd24fe6b90555732ad5dee497b8a28a1cd7
Oma-yma1 <zgani.omayma@gmail.com>
2023-07-11 09:40:24 -0700

    task4110

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -9,13 +9,14 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self, attrs=None):
         """retrieves a dictionary representation"""
         if attrs is None:
             return self.__dict__
         else:
-            json_dic = {}
+            jso_dic = {}
             for attr in attrs:
                 if hasattr(self, attr):
-                    json_dic[attr] =getattr(self, attr)
-            return json_dic
+                    jso_dic[attr] = getattr(self, attr)
+            return jso_dic
