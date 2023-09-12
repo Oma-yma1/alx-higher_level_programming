ea5313fae1f4c7c3fa9caee7692b8fa4a2247aae
Oma-yma1 <zgani.omayma@gmail.com>
2023-09-12 09:51:52 -0700

    task7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let coun = 0;
+  list.forEach(element => {
+    if (element === searchElement) coun++;
+  });
+return coun;
+};
