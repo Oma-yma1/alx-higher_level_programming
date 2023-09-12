a15810c30a664315bb8bfafe250bee631d17925b
Oma-yma1 <zgani.omayma@gmail.com>
2023-09-12 08:20:12 -0700

    task3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(h) || isNaN(w)) return;
+    this.width = w;
+    this.height = h;
+  }
+  print () {
+    for (let j = 0; j < this.height; j++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
