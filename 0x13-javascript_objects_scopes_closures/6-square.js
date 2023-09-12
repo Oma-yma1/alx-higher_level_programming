4ba7ea062f0fcc38164ee6318ccb603c9f65fea5
Oma-yma1 <zgani.omayma@gmail.com>
2023-09-12 09:46:00 -0700

    task6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square.js') {
-  constructor (size) {
-    super(size, size);
-  }
   charPrint (c) {
-    this.print(c);
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let j = 0; j < this.height; j++) console.log(c.repeat(this.width));
+    }
   }
 };
