880d68d24010f1ad7fb7a0a7d5bfaf31213698ad
Oma-yma1 <zgani.omayma@gmail.com>
2023-09-12 07:55:51 -0700

    task2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) return;
+    if (w <= 0 || h <= 0 || isNaN(h) || isNaN(w)) return;
     this.width = w;
     this.height = h;
   }
