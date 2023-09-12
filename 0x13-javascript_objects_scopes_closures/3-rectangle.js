ad53c393bdd62082aeaedd1d2f6642e54d7ecb9d
Oma-yma1 <zgani.omayma@gmail.com>
2023-09-12 08:22:40 -0700

    task3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,6 +5,7 @@ module.exports = class Rectangle {
     this.width = w;
     this.height = h;
   }
+
   print () {
     for (let j = 0; j < this.height; j++) {
 	    console.log('X'.repeat(this.width));
