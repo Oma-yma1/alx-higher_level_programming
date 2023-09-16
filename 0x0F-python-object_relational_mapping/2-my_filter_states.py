88def9d41d59febdb04eaf576f82a151f4435bba
Oma-yma1 <zgani.omayma@gmail.com>
2023-09-16 07:51:04 -0700

    task2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,11 +2,15 @@
 """script that list states from the database hbtn_0e_0_usa"""
 import MySQLdb
 from sys import argv
+
+
 if __name__ == "__main__":
     """lists from the database"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY\
+                 states.id ASC".format(argv[4]))
     rows = cur.fetchall()
     for i in rows:
         print(i)
