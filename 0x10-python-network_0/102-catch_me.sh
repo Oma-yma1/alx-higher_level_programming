4536856d201536381d020cb46b743078af972b1f
Oma-yma1 <zgani.omayma@gmail.com>
2023-09-28 09:22:56 -0700

    task

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes request to 0.0.0.0:5000/catch_me
-curl -sLX PUT 0.0.0.0:5000/catch_me -H "Origin: You got me!" --data "user_id=98"
+curl -sLX PUT 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool" --data "user_id=98"
