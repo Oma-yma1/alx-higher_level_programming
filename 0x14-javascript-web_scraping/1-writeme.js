741cc9708f0a1b05c89da73c2b119d318b871e88
Oma-yma1 <zgani.omayma@gmail.com>
2023-10-24 08:42:54 -0700

    task1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filepath = process.argv[2];
+const str = process.argv[3];
+fs.writeFile(filepath, str, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
